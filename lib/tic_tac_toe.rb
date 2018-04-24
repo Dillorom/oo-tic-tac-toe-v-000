1eb0390d60acc547b23c6b7a8b50d16ebf8d1049
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-24 22:24:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ class TicTacToe
     end
   end
 
-  def valid_move?(board, index)
-   if !position_taken?(board, index) && index.between?(0,8)
+  def valid_move?(index)
+   if !position_taken?(index) && index.between?(0,8)
       return true
    end
   end

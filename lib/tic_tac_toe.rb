a401006f1b5d151bcf41a5a1f4c120ee68a7c8de
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-24 22:13:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,114 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
+
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  display_board
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(board, position, player_token)
+    board[position] = player_token
+    end
+    move(board, 0, "O")
+    move(board, 8, "X")
+
+  def position_taken?(board, position)
+    if board[position] == " " || board[position] == "" || board[position] == nil
+      false
+    elsif board[position] == "X" || board[position] =- "O"
+      true
+    end
+  end
+
+  def valid_move?(board, index)
+   if !position_taken?(board, index) && index.between?(0,8)
+      return true
+   end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    users_input=gets.strip
+    index = input_to_index(users_input)
+    if valid_move?(board, index)
+  <<<<<<< HEAD
+      move(board, index, current_player(board))
+  =======
+      move(board, index, player_character ="X")
+  >>>>>>> fbdcfddebea01753e2e149aca817fc7076660ee9
+      display_board
+    else turn
+    end
+  end
+
+  def turn_count
+    counter = 0
+    board.count do |count|
+      if count != " "
+        counter+=1
+      end
+    end
+  end
+
+  def current_player
+    if turn_count % 2 == 0
+     "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combination|
+      position_1 = board[win_combination[0]]
+      position_2 = board[win_combination[1]]
+      position_3 = board[win_combination[2]]
+
+      position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_combination[0])
+      end
+  end
+
+  def full?
+    board.all? do |position|
+      position == "X" || position == "O"
+    end
+  end
+
+  def draw?
+     if full? && !won?
+       true
+     end
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def winner
+    if won?
+      board[won?[0]]
+    end
+  end
+
+  def play
+    until over?
+    turn
+    end
+
+      if won?
+       puts "Congratulations #{winner(board)}!"
+      elsif draw?
+       puts "Cat's Game!"
+   end
+
+  
 end
